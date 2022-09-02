5cbe481093cf93e25b3ff91a7e8ce1300576bc13
Karola93 <95715020+Karola93@users.noreply.github.com>
2022-09-02 16:26:08 +0200

    chore(index.js): add Provider

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App/App';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import store from "./App/store/store";
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
+        <Provider store={store}>
+            <React.StrictMode>
+                <App/>
+            </React.StrictMode>
+        </Provider>
     </BrowserRouter>
-
 );
 
 reportWebVitals();
