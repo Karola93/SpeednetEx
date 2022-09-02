80ad7f074573d5911eedce03c7013ae4ad849d65
Karola93 <95715020+Karola93@users.noreply.github.com>
2022-09-02 16:35:01 +0200

    feat(Detail): add another detail to display

--- src/App/components/Detail.js
+++ src/App/components/Detail.js
@@ -5,8 +5,10 @@ import {selectCheckboxId} from "../store/checkboxSlice";
 const Detail = ({data}) => {
     const id = useSelector(selectCheckboxId);
     const checked = data.filter(el => el["id"] === id)[0];
-    return (
-        <h1>{checked['subject']}</h1>
+    return ( <>
+            <h1>{checked['subject']}</h1>
+        {checked['snippet'] && <h2>{checked['snippet']}</h2>}
+    </>
     )
 };
 
