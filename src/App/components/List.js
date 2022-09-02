4b195ae031d114e849805adb244b76f8eea03515
Karola93 <95715020+Karola93@users.noreply.github.com>
2022-09-02 16:08:40 +0200

    feat(List): get isChecked and id change on click

--- src/App/components/List.js
+++ src/App/components/List.js
@@ -1,14 +1,23 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
+import {useDispatch} from "react-redux";
 import './List.css';
+import {handleCheck, handleCheckboxId} from "../store/checkboxSlice";
 
 const List = ({data}) => {
+    const dispatch = useDispatch();
     const navigate = useNavigate();
 
     const checkboxChangeHandler = (e) => {
         e.preventDefault();
+        dispatch(handleCheck());
         navigate('/detail');
-    }
+    };
+
+    const getIdHandler = (id) => {
+        checkboxChangeHandler()
+        dispatch(handleCheckboxId(id))
+    };
 
     return (
         <ul className="list">
@@ -16,7 +25,7 @@ const List = ({data}) => {
                 data.map( (el) => {
                     return <li key={el['id']}>
                         <label>
-                            <input key={el['id']} type="checkbox" onClick={checkboxChangeHandler}/> {el['from']}
+                            <input key={el['id']} type="checkbox" onClick={() => getIdHandler(el['id'])}/> {el['from']}
                         </label>
                     </li>
                 })
