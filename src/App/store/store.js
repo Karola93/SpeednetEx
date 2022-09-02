2ef871c4c050af04480622e1f3d12aa1b707754c
Karola93 <95715020+Karola93@users.noreply.github.com>
2022-09-02 15:54:19 +0200

    feat(store): create store

---
+++ src/App/store/store.js
@@ -0,0 +1,9 @@
+import {configureStore} from "@reduxjs/toolkit";
+import checkboxReducer from './checkboxSlice';
+
+const store = configureStore({
+    reducer: { checkbox: checkboxReducer }
+});
+
+export const AppDispatch = typeof store.dispatch;
+export default store;
